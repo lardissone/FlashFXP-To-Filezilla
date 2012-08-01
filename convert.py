bb5c5d65c4447097c04ff9da874e361deb9c0387
appel <appel@alsjeblaft.nl>
2012-08-01 15:32:42 -0400

    Set default FTP port value to '21'.

--- convert.py
+++ convert.py
@@ -29,6 +29,7 @@ def main():
             if node.getElementsByTagName('ADDRESS') else ''
         port = node.getElementsByTagName('PORT')[0].firstChild.data \
             if node.getElementsByTagName('PORT') else ''
+            if node.getElementsByTagName('PORT') else 21
         user = node.getElementsByTagName('USERNAME')[0].firstChild.data \
             if node.getElementsByTagName('USERNAME') else ''
         passw = node.getElementsByTagName('PASSWORD')[0].firstChild.data \
