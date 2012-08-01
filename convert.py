74b406afd821a856557ca9a0cafcf1896c3869b0
Leandro Ardissone <lardissone@gmail.com>
2012-08-01 14:36:12 -0300

    Fixed issue with missing fields. Thanks @appel

--- convert.py
+++ convert.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import os
 from xml.dom import minidom
-import sys 
+import sys
+
 
 def main():
     flash_file = sys.argv[1]
-    
+
     xml_file = os.path.abspath(__file__)
     xml_file = os.path.dirname(xml_file)
     xml_file = os.path.join(xml_file, flash_file)
-    
+
     try:
         xmldoc = minidom.parse(xml_file)
     except Exception, inst:
@@ -17,19 +18,22 @@ def main():
         print "-Please, be sure that this file exists: %s" % xml_file
         print inst
         return
-    
+
     nodes = xmldoc.getElementsByTagName('SITE')
-    
+
     xml_str = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><FileZilla3><Servers>'
-    
+
     for node in nodes:
         name = node.attributes['NAME'].value
-        # print node.getElementsByTagName('PROTOCOL')[0].firstChild.data
-        server = node.getElementsByTagName('ADDRESS')[0].firstChild.data
-        port = node.getElementsByTagName('PORT')[0].firstChild.data
-        user = node.getElementsByTagName('USERNAME')[0].firstChild.data
-        passw = node.getElementsByTagName('PASSWORD')[0].firstChild.data
-        
+        server = node.getElementsByTagName('ADDRESS')[0].firstChild.data \
+            if node.getElementsByTagName('ADDRESS') else ''
+        port = node.getElementsByTagName('PORT')[0].firstChild.data \
+            if node.getElementsByTagName('PORT') else ''
+        user = node.getElementsByTagName('USERNAME')[0].firstChild.data \
+            if node.getElementsByTagName('USERNAME') else ''
+        passw = node.getElementsByTagName('PASSWORD')[0].firstChild.data \
+            if node.getElementsByTagName('PASSWORD') else ''
+
         xml_str += '<Server>'
         xml_str += '<Host>' + str(server) + '</Host>'
         xml_str += '<Port>' + str(port) + '</Port>'
@@ -43,18 +47,18 @@ def main():
         xml_str += '<MaximumMultipleConnections>0</MaximumMultipleConnections>'
         xml_str += '<EncodingType>Auto</EncodingType>'
         xml_str += '<BypassProxy>0</BypassProxy>'
-        xml_str += '<Name>'+ str(name) +'</Name>'
+        xml_str += '<Name>' + str(name) + '</Name>'
         xml_str += '<Comments></Comments>'
         xml_str += '<LocalDir></LocalDir>'
         xml_str += '<RemoteDir></RemoteDir>'
         xml_str += '<SyncBrowsing>0</SyncBrowsing>'
         xml_str += '</Server>'
-        
+
     xml_str += '</Servers></FileZilla3>'
-    
+
     xml_output_file = open('filezilla.xml', 'w')
     xml_output_file.write(xml_str)
     xml_output_file.close()
 
 if __name__ == "__main__":
-	main()
+    main()
